c5f84f6089c8d83cecebd360bc4210e631fd2587
booneng <ohbooneng@gmail.com>
2020-09-04 15:44:58 +0800

    remove generated protos

--- scripts/generate_protos.sh
+++ scripts/generate_protos.sh
@@ -14,9 +14,9 @@ docker run \
     --volume "${ROOT}:${ROOT}" \
     --workdir "${ROOT}" \
     "${PROTOC_GO_CONTAINER_IMAGE}" \
-        --proto_path=${ROOT}/proto \
-        --go_out=plugins=grpc,paths=source_relative:./proto/gen \
-        ${ROOT}/proto/*.proto
+        --proto_path=${ROOT}/protobuf \
+        --go_out=plugins=grpc,paths=source_relative:./nowagengo \
+        ${ROOT}/protobuf/*.proto
 
 docker run \
     --interactive \
@@ -24,9 +24,9 @@ docker run \
     --volume "${ROOT}:${ROOT}" \
     --workdir "${ROOT}" \
     "${PROTOC_DART_CONTAINER_IMAGE}" \
-        --proto_path=${ROOT}/proto \
-        --dart_out=grpc:./proto/gen \
-        ${ROOT}/proto/*.proto
+        --proto_path=${ROOT}/protobuf \
+        --dart_out=grpc:./nowagendart \
+        ${ROOT}/protobuf/*.proto
 
 docker run \
     --interactive \
@@ -34,11 +34,11 @@ docker run \
     --volume "${ROOT}:${ROOT}" \
     --workdir "${ROOT}" \
     "${PROTOC_SWIFT_CONTAINER_IMAGE}" \
-        --proto_path=${ROOT}/proto \
+        --proto_path=${ROOT}/protobuf \
         --swift_opt=Visibility=Public \
-        --swift_out=./proto/gen \
+        --swift_out=./nowagenswift \
         --grpc-swift_opt=Visibility=Public \
-        --grpc-swift_out=./proto/gen \
-        ${ROOT}/proto/*.proto
+        --grpc-swift_out=nowagenswift \
+        ${ROOT}/protobuf/*.proto
 
 echo "Protos regenerated (OK)"
